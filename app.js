3eb794ced56007872afd6b67aac9924e0a7d1a9c
JeisonGuerrero <jeison.g.s@hotmail.com>
2025-03-19 23:31:28 -0500

    Validaciones 2

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ function validarInputs() {
     const email = document.getElementById('email').value.trim();
     const number = document.getElementById('number').value.trim();
     const texto = document.getElementById('texto').value.trim();
+    const soloNumeros = /^[0-9]+$/;
+    const soloLetras = /^[a-zA-Z\s]+$/;
 
     if (!nombre) {
         alert('El campo nombre no puede estar vacío');
+        return false;
+    }
+    if (!soloLetras.test(nombre)) {
+        alert('El campo nombre solo puede contener letras');
         return false;
     }
     if (!email) {
         alert('El campo email no puede estar vacío');
         return false;
     }
-    if (!number) {
-        alert('El campo número no puede estar vacío');
+    if (!number || number.length > 11) {
+        alert('El campo número no puede estar vacío y solo puede contener hasta 10 numeros');
+        return false;
+    }
+    if (!soloNumeros.test(number)) {
+        alert('El campo número solo puede contener números');
         return false;
     }
     if (!texto) {
